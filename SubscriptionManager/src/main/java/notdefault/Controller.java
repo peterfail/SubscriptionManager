4c2711854af29b6155d834eb70ec96a26f85e1a1
peterfail <madsb17@gmail.com>
2021-07-06 00:32:30 +0200

    Add files via upload

--- SubscriptionManager/src/main/java/notdefault/Controller.java
+++ SubscriptionManager/src/main/java/notdefault/Controller.java
@@ -1,16 +1,11 @@
 package notdefault;
 
-import java.util.ArrayList;
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.json.JSONException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class Controller {
-    private static final String template = "Hello, %s!";
-    private final AtomicLong counter = new AtomicLong();
     Subscriptions[] subscriptionsarray = new Subscriptions[10];
     @GetMapping("/subscriptions/{id}")
     public ResponseEntity<Subscriptions> getsubscriptions(@PathVariable("id") int id) throws JSONException {
@@ -24,6 +19,5 @@ public class Controller {
     @DeleteMapping("/subscriptions/{id}")
     public void deletesubscriptions(@PathVariable int id) {
         subscriptionsarray[id]=null;
-
     }
 }
